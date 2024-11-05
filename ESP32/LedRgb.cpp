6e99c00b75a1eaf574bf28009bd2dae9ff835b1c
luckymls <melis.luca2014@gmail.com>
2024-11-05 16:33:10 +0100

    led rgb class implemented

--- ESP32/LedRgb.cpp
+++ ESP32/LedRgb.cpp
@@ -1,1 +1,31 @@
 #include "LedRgb.h"
+
+LedRgb::LedRgb(int redPin, int greenPin, int bluePin){
+
+    this->redPin = redPin;
+    this->greenPin = greenPin;
+    this->bluePin = bluePin;
+
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+
+    off();
+}
+
+void LedRgb::setColor(int rVal, int gVal, int bVal){
+    analogWrite(this->redPin, rVal);
+    analogWrite(this->greenPin, gVal);
+    analogWrite(this->bluePin, bVal);
+}
+
+void LedRgb::off(){
+    analogWrite(this->redPin, 0);
+    analogWrite(this->greenPin, 0);
+    analogWrite(this->bluePin, 0);
+}
+
+
+
+
+
--- ESP32/LedRgb.h
+++ ESP32/LedRgb.h
@@ -1,5 +1,18 @@
 #ifndef LEDRGB_H
 #define LEDRGB_H
 
+#include <Arduino.h>
+
+class LedRgb {
+    private:
+        int redPin;
+        int greenPin;
+        int bluePin;
+
+    public:
+        LedRgb(int rPin, int gPin, int bPin);
+        void setColor(int rVal, int gVal, int bVal);
+        void off();
+};
 
 #endif
