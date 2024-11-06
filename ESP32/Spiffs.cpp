532dda536aebcb3b0740b421d0b0d34488e03241
luckymls <melis.luca2014@gmail.com>
2024-11-06 10:41:26 +0100

    Spiffs files added

--- ESP32/Spiffs.cpp
+++ ESP32/Spiffs.cpp
@@ -1,1 +1,70 @@
 #include "Spiffs.h"
+
+Spiffs::Spiffs() {}
+
+bool Spiffs::begin() {
+
+    if (!SPIFFS.begin(true))
+        return false;    
+    return true;
+}
+
+bool Spiffs::writeFile(const char* path, const char* message) {
+
+    File file = SPIFFS.open(path, FILE_WRITE);
+
+    if (!file) {
+        return false;
+    }
+
+    if (!file.print(message)) {
+        file.close();
+        return false;
+    }
+    
+    file.close();
+    return true;
+}
+
+String Spiffs::readFile(const char* path) {
+
+    File file = SPIFFS.open(path, FILE_READ);
+    String content;
+
+    if (!file)
+        return String(); // failed
+    
+    while (file.available()) {
+        content += char(file.read());
+    }
+
+    file.close();
+    return content;
+}
+
+bool Spiffs::appendToFile(const char* path, const char* message) {
+
+    File file = SPIFFS.open(path, FILE_APPEND);
+
+    if (!file) {
+        Serial.printf("Failed to open file %s for appending\n", path);
+        return false;
+    }
+
+    if (!file.print(message)) {
+        file.close();
+        return false;
+    }
+
+    file.close();
+    return true;
+}
+
+bool Spiffs::deleteFile(const char* path) {
+
+    if (SPIFFS.remove(path)) 
+        return true;
+    
+    return false;
+    
+}
--- ESP32/Spiffs.h
+++ ESP32/Spiffs.h
@@ -1,5 +1,18 @@
 #ifndef SPIFFS_H
 #define SPIFFS_H
 
+#include <Arduino.h>
+#include <SPIFFS.h>
+
+class Spiffs {
+public:
+    Spiffs();                       
+    bool begin();                   // Initialize SPIFFS
+    bool writeFile(const char* path, const char* message); // Write to a file
+    String readFile(const char* path); // Read from a file
+    bool appendToFile(const char* path, const char* message); // Append to a file
+    bool deleteFile(const char* path); // Delete a file
+};
 
 #endif
+
